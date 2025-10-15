9e9bb958a6d5156e19478980cd79aa82e370e020
JoshuaParungao <jmphotography1830@gmail.com>
2025-10-15 09:07:49 +0800

    second commit

--- MyBackEnd/registration/views.py
+++ MyBackEnd/registration/views.py
@@ -10,9 +10,17 @@ from rest_framework.response import Response
 
 @api_view(['POST'])
 def register_user(request):
-        serializer = UserRegistrationSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-  
+  serializer = UserRegistrationSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+    return Response(serializer.data, status=status.HTTP_201_CREATED)
+  return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+def list_user(request):
+  # Retrieve all users and return serialized data
+  users = UserRegistration.objects.all()
+  serializer = UserRegistrationSerializer(users, many=True)
+  return Response(serializer.data, status=status.HTTP_200_OK)
+        
